f5871e136fd1f86ccecfeed06cef5ae585465f15
tsungjen <max.hsu@ironyun.com>
2017-05-10 15:52:46 +0800

    fixed: octave g++ compilation

--- compile.m
+++ compile.m
@@ -12,8 +12,10 @@ function compile
 cd 3rd_party/libsvm-3.20/matlab
 make
 cd ../../../
-mkoctfile --mex -DMATLAB_MEX_FILE imResampleMex.cpp
 
+mkoctfile --mex -DMATLAB_MEX_FILE imResampleMex.cpp
+mkoctfile --mex -DMATLAB_MEX_FILE warp.cpp
+mkoctfile --mex -DMATLAB_MEX_FILE lk.cpp -I/home/max/opencv/include/opencv
 
 include = ' -I/usr/local/include/opencv/ -I/usr/local/include/ -I/usr/include/opencv/';
 lib = ' -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_video';

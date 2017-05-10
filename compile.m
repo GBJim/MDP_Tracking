fea54878db8cd9ca226525a993e200af7d0c1182
tsungjen <max.hsu@ironyun.com>
2017-05-10 15:44:05 +0800

    transcode: auto setup environment

--- compile.m
+++ compile.m
@@ -9,6 +9,12 @@
 % change the include and lib path if necessary
 function compile
 
+cd 3rd_party/libsvm-3.20/matlab
+make
+cd ../../../
+mkoctfile --mex -DMATLAB_MEX_FILE imResampleMex.cpp
+
+
 include = ' -I/usr/local/include/opencv/ -I/usr/local/include/ -I/usr/include/opencv/';
 lib = ' -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_video';
 eval(['mex lk.cpp -O' include lib]);
